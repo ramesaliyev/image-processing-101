e4bc7e50c13580f1ea18b9822298afda7b0d7a24
Rames Aliyev <ramesaliyev@gmail.com>
2021-10-11 11:49:13 +0300

    add histogram equalization

--- lib/pgm.py
+++ lib/pgm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def read(name):
     with open(name) as f:
@@ -31,3 +32,28 @@ def show(pgm, cmap='gray'):
     
 def load_and_show(path):
     show(read(path))
+    
+def calculate_histogram(pgm):
+    (pixels, (width, height), maxval) = pgm;
+    n = width * height;
+    
+    freqs = [0] * 256;
+    for color in pixels:
+        freqs[color] += 1;
+    
+    intensity = [freq/n for freq in freqs]
+    
+    return (freqs, intensity);
+
+def equalize_histogram(pgm, L = 256):
+    pixels, *_ = pgm
+    _, intensity = calculate_histogram(pgm)
+    newlevels = [0] * L
+    
+    cdf = 0
+    for i, prob in enumerate(intensity):
+        cdf += prob
+        newlevels[i] = math.floor(cdf * (L-1))
+        
+    for i, color in enumerate(pixels):
+        pixels[i] = newlevels[color]
